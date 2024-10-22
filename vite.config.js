3181d7db2a85a6eae51988e12b02275173030d18
Oscar Lozano <olozano1194@gmail.com>
2024-10-21 19:11:40 -0500

    modificando estos dos archivos para tratar de que funcione en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Olozano1194.github.io/portafolio'
+  base: '/portafolio/'
 })
