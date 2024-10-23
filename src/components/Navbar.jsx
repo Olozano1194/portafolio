818ea7241c3aa52d70d050fc0e3c48ad0c0316fc
Oscar Lozano <olozano1194@gmail.com>
2024-10-22 21:57:14 -0500

    se arreglaron algunos estilos en la parte del header para que se viera bien en diferentes dimensiones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import logo from '../assets/logoOscar1.jpeg';
 function Navbar() {  //1094
     return (
         <>
-            <div className='flex items-center pl-4 gap-2 lg:w-8/12 md:p-3'>
+            <div className='lg:flex lg:items-center lg:gap-2 lg:w-8/12 lg:p-3'>
                 <img
                 className='logo rounded-lg size-9' 
                 src={logo}
@@ -16,13 +16,13 @@ function Navbar() {  //1094
             </div>
             
             <nav className='menu flex items-center justify-center gap-2 text-stone-50 md:gap-4 lg:justify-end lg:w-full'>
-                <Link to='skill' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] md:pl-5 lg:text-[1.2rem]' >Sobre Mi</Link>
-                <Link to='skill' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem]' >Skills</Link>
-                <Link to='softSkill' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem]' >Soft Skill</Link>
-                <Link to='hobbies' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem]'>Hobbies</Link>
-                <Link to='formacionAcademica' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem]' >Formación</Link>
-                <Link to='proyectos' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem]' >Proyectos</Link>
-                <Link to='contacto' smooth={true} duration={500} className='linkNav text-[0.8rem] md:text-[1.5rem] lg:text-[1.2rem] lg:pr-3 ' >Contacto</Link>
+                <Link to='skill' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] md:pl-3 lg:text-[1.1rem]' >Sobre Mi</Link>
+                <Link to='skill' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] lg:text-[1.1rem]' >Skills</Link>
+                <Link to='softSkill' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] lg:text-[1.1rem]' >Soft Skill</Link>
+                <Link to='hobbies' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] lg:text-[1.1rem]'>Hobbies</Link>
+                <Link to='formacionAcademica' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.1rem] lg:text-[1.2rem]' >Formación</Link>
+                <Link to='proyectos' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] lg:text-[1.1rem]' >Proyectos</Link>
+                <Link to='contacto' smooth={true} duration={500} className='linkNav text-[0.78rem] md:text-[1.3rem] lg:text-[1.1rem] lg:pr-2 ' >Contacto</Link>
                         
             </nav>
         </>
